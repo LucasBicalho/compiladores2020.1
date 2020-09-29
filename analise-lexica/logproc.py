422ed37f503b0d734eb62eaeb82417a0cd6148af
Lucas Bicalho <lucasbicalho@id.uff.br>
2020-09-29 20:34:35 -0300

    exercicio respondido

--- analise-lexica/logproc.py
+++ analise-lexica/logproc.py
@@ -12,15 +12,18 @@ tokens = [
 
 def t_TIMESTAMP(t):
     # Regular expression for TIMESTAMP
+    r'\d{2}:\d{2}:\d{2}\.\d{6}\s-\d{4}'
     return t
 
 def t_PROC(t):
     # Regular expression for PROC
+    r'\t.*\t'
     t.value = t.value[1:len(t.value) - 1]
     return t
 
 def t_MESSAGE(t):
     # Regular expression for MESSAGE
+    r'.+\n(((\D)|(\d+\s)).*\n\n?)*'
     t.value = t.value[:len(t.value) - 1]
     return t
 
@@ -41,8 +44,25 @@ class LogProcLexer:
         self.lexer.input(self.data)
 
     def collect_messages(self):
-        # Do your magic here
+        kernel_messages = []
+        while True:
+            token = self.lexer.token()
+
+            if not token:
+              break
+            
+            if token.type == 'PROC' and token.value == 'kernel':
+              message = self.lexer.token()
+              kernel_messages.append(message)
+
+        return kernel_messages
         
+def print_array(array):
+    for token in array:
+        if len(token.value) >= 40:
+            token.value = token.value[:20] + '  (...)  ' + token.value[-10:]
+        print(token)
+
 if __name__ == '__main__':
     print(LogProcLexer().collect_messages())
     
